803adfdd35ce6c6da0f48396e6a50b39138a92c1
RunninglVlan <RedCapShortCut@gmail.com>
2018-09-19 19:22:45 +0300

    Created ScriptableObject and its instance   Reformatting

--- 03-Text101/Assets/AdventureGame.cs
+++ 03-Text101/Assets/AdventureGame.cs
@@ -5,7 +5,8 @@ using UnityEngine.UI;
 
 public class AdventureGame : MonoBehaviour {
 
-    [SerializeField] Text textComponent;
+    [SerializeField]
+    Text textComponent;
 
     void Start() {
         textComponent.text = "My story text";
---
+++ 03-Text101/Assets/State.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject {
+
+	[TextArea(3, 10)]
+	[SerializeField]
+	string storyText;
+}
