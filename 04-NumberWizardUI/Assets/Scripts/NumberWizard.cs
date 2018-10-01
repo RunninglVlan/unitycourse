2fdf6be8c7250812c6d65ef23fb765c7529d4ed0
RunninglVlan <RedCapShortCut@gmail.com>
2018-10-02 01:45:29 +0300

    Added NumberWizard logic   Removed all prints   Replaced Update with 2 methods which were attached to onClicks   Serialized 3 fields - min/max, guessText

---
+++ 04-NumberWizardUI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using TMPro;
+
+class NumberWizard : MonoBehaviour
+{
+
+    [SerializeField]
+    int max;
+    [SerializeField]
+    int min;
+
+    [SerializeField]
+    TextMeshProUGUI textComponent;
+
+    private int guess;
+
+    void Start()
+    {
+        nextGuess();
+    }
+
+    public void setMinToGuess()
+    {
+        min = guess;
+        nextGuess();
+    }
+
+    public void setMaxToGuess()
+    {
+        max = guess;
+        nextGuess();
+    }
+
+    private void nextGuess()
+    {
+        int previousGuess = guess;
+        for (int i = 0; i < max - min; i++)
+        {
+            int nextGuess = Mathf.FloorToInt(Random.Range(min, max + 1));
+            if (previousGuess != nextGuess)
+            {
+                setGuess(nextGuess);
+                return;
+            }
+        }
+    }
+
+    private void setGuess(int guess)
+    {
+        this.guess = guess;
+        textComponent.text = guess.ToString();
+    }
+}
