66e224e971d2d9f4cf7a4dbc0da509f523dabab8
RunninglVlan <RedCapShortCut@gmail.com>
2018-10-03 21:37:40 +0300

    Made ball be launchable on mouse click   Serialized launchImpulse

--- 05-BlockBreaker/Assets/Scripts/Ball.cs
+++ 05-BlockBreaker/Assets/Scripts/Ball.cs
@@ -6,17 +6,37 @@ public class Ball : MonoBehaviour
 {
 
     [SerializeField] Paddle paddle;
+    [SerializeField] Vector2 launchImpulse;
 
     private Vector2 distanceToPaddle;
+    private bool ballLaunched = false;
 
     void Start()
     {
-	distanceToPaddle = transform.position - paddle.transform.position;
+        distanceToPaddle = transform.position - paddle.transform.position;
     }
 
     void Update()
     {
+        if (!ballLaunched)
+        {
+            stickToThePaddle();
+            launchOnMouseClick();
+        }
+    }
+
+    private void stickToThePaddle()
+    {
         var paddlePosition = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
         transform.position = paddlePosition + distanceToPaddle;
+    }
+
+    private void launchOnMouseClick()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            ballLaunched = true;
+            GetComponent<Rigidbody2D>().AddForce(launchImpulse, ForceMode2D.Impulse);
+        }
     }
 }
