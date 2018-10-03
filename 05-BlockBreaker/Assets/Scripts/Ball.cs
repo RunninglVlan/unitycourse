2d45c1911e3a52b04e60f4f16507db1b83d12522
RunninglVlan <RedCapShortCut@gmail.com>
2018-10-03 20:03:54 +0300

    Made ball stick to the paddle   Added script, calculated distance, added it to paddlePosition   Moved paddle and ball to start positions

---
+++ 05-BlockBreaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    [SerializeField] Paddle paddle;
+
+    private Vector2 distanceToPaddle;
+
+    void Start()
+    {
+	distanceToPaddle = transform.position - paddle.transform.position;
+    }
+
+    void Update()
+    {
+        var paddlePosition = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+        transform.position = paddlePosition + distanceToPaddle;
+    }
+}
