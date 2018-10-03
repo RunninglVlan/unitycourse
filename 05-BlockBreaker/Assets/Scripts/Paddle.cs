368c9bf31de23e4788944feb6247701dda985166
RunninglVlan <RedCapShortCut@gmail.com>
2018-10-03 19:11:57 +0300

    Created script for moving paddle with mouse   FIxed typo   Disabled next level button for now

---
+++ 05-BlockBreaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+    private float cameraWidth;
+
+    void Start()
+    {
+        var camera = Camera.main;
+        cameraWidth = camera.orthographicSize * 2 * camera.aspect;
+    }
+
+    void Update()
+    {
+        var mousePositionX = Input.mousePosition.x / Screen.width * cameraWidth;
+        var newPosition = new Vector2(mousePositionX, transform.position.y);
+        transform.position = newPosition;
+    }
+}
