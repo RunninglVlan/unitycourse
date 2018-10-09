fd5b35f4159eb7bfd549797af506d550f9e29100
RunninglVlan <RedCapShortCut@gmail.com>
2018-10-09 11:35:08 +0300

    Refactoring - removed unneeded code   Previously thought for some reason that Random returns float

--- 02-NumberWizard/Assets/NumberWizard.cs
+++ 02-NumberWizard/Assets/NumberWizard.cs
@@ -1,61 +1,74 @@
 ﻿using UnityEngine;
 
-class NumberWizard : MonoBehaviour {
+class NumberWizard : MonoBehaviour
+{
 
-	private const int MAX = 1000;
-	private const int MIN = 0;
+    private const int MAX = 1000;
+    private const int MIN = 0;
 
-	private int max;
-	private int min;
-	private int guess;
+    private int max;
+    private int min;
+    private int guess;
 
-	void Start() {
-		max = MAX;
-		min = MIN;
+    void Start()
+    {
+        max = MAX;
+        min = MIN;
 
-		print("Welcome to Number Wizard");
-		print("Pick a number, don't tell me what it is...");
+        print("Welcome to Number Wizard");
+        print("Pick a number, don't tell me what it is...");
 
-		print($"The highest number you can pick is {MAX}");
-		print($"The lowest number you can pick is {MIN}");
+        print($"The highest number you can pick is {MAX}");
+        print($"The lowest number you can pick is {MIN}");
 
-		nextGuess();
-		printQuestion();
-		print("Press UP arrow for higher, DOWN arrow for lower, ENTER for equal");
-	}
+        nextGuess();
+        printQuestion();
+        print("Press UP arrow for higher, DOWN arrow for lower, ENTER for equal");
+    }
 
-	void Update() {
-		if (Input.GetKeyDown(KeyCode.UpArrow)) {
-			min = guess;
-			nextGuess();
-			printQuestion();
-		} else if (Input.GetKeyDown(KeyCode.DownArrow)) {
-			max = guess;
-			nextGuess();
-			printQuestion();
-		} else if (Input.GetKeyDown(KeyCode.Return)) {
-			printAnswer();
-		}
-	}
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess;
+            nextGuess();
+            printQuestion();
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = guess;
+            nextGuess();
+            printQuestion();
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            printAnswer();
+        }
+    }
 
-	private void printQuestion() {
-		print($"Is the number higher [↑] or lower [↓] than {guess}? Maybe it is equal [⏎]?");
-	}
+    private void printQuestion()
+    {
+        print($"Is the number higher [↑] or lower [↓] than {guess}? Maybe it is equal [⏎]?");
+    }
 
-	private void nextGuess() {
-		int previousGuess = guess;
-		for (int i = 0; i < max - min; i++) {
-			int nextGuess = Mathf.FloorToInt(Random.Range(min, max + 1));
-			if (previousGuess != nextGuess) {
-				guess = nextGuess;
-				return;
-			}
-		}
-		printAnswer();
-	}
+    private void nextGuess()
+    {
+        int previousGuess = guess;
+        for (int i = 0; i < max - min; i++)
+        {
+            int nextGuess = Random.Range(min, max + 1);
+            if (previousGuess != nextGuess)
+            {
+                guess = nextGuess;
+                return;
+            }
+        }
+        printAnswer();
+    }
 
-	private void printAnswer() {
-		print($"You picked {guess}");
-		Start();
-	}
+    private void printAnswer()
+    {
+        print($"You picked {guess}");
+        Start();
+    }
 }
