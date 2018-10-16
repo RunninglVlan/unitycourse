c87f51d8889c9521ecccbf80fba84e6ccc4e6eb3
RunninglVlan <RedCapShortCut@gmail.com>
2018-10-16 11:43:20 +0300

    Added control of gameSpeed through timeScale   Created new Class GameStatus, made a prefab of the same name   Added prefab to levels   Reduced gravity a bit

---
+++ 05-BlockBreaker/Assets/Scripts/GameStatus.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatus : MonoBehaviour
+{
+
+    [Range(0.1f, 10)]
+    [SerializeField]
+    float gameSpeed = 1;
+
+    void Start()
+    {
+        Time.timeScale = gameSpeed;
+    }
+}
