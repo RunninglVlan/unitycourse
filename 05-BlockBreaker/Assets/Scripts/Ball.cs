d099022847a4b2d8861ee9b8359f2aa17c9d1a15
RunninglVlan <RedCapShortCut@gmail.com>
2018-10-17 23:05:13 +0300

    Added velocityRandomFactor to ball's movement   To prevent it from getting stuck on axes   Disabled gravity for real

--- 05-BlockBreaker/Assets/Scripts/Ball.cs
+++ 05-BlockBreaker/Assets/Scripts/Ball.cs
@@ -8,6 +8,9 @@ public class Ball : MonoBehaviour
     [SerializeField] Paddle paddle;
     [SerializeField] Vector2 launchImpulse;
     [SerializeField] AudioClip[] collisionSounds;
+    [SerializeField] float velocityRandomFactor = 0.2f;
+
+    new private Rigidbody2D rigidbody2D;
 
     private Vector2 distanceToPaddle;
     private bool isLaunched = false;
@@ -15,14 +18,17 @@ public class Ball : MonoBehaviour
     void Start()
     {
         distanceToPaddle = transform.position - paddle.transform.position;
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     void OnCollisionEnter2D(Collision2D _)
     {
+        var velocityTweak = new Vector2(Random.Range(0, velocityRandomFactor), Random.Range(0, velocityRandomFactor));
         if (isLaunched)
         {
             AudioClip sound = collisionSounds[Random.Range(0, collisionSounds.Length)];
             GetComponent<AudioSource>().PlayOneShot(sound);
+            rigidbody2D.velocity += velocityTweak;
         }
     }
 
@@ -46,7 +52,7 @@ public class Ball : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             isLaunched = true;
-            GetComponent<Rigidbody2D>().AddForce(launchImpulse, ForceMode2D.Impulse);
+            rigidbody2D.AddForce(launchImpulse, ForceMode2D.Impulse);
             paddle.GetComponent<AudioSource>().Play();
         }
     }
