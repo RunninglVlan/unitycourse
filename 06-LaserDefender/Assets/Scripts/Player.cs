9ab0f5313818da10b7e7abd955cdf859dbcb233f
RunninglVlan <RedCapShortCut@gmail.com>
2018-10-21 21:50:53 +0300

    Refactoring - rewrote code to be more compact

--- 06-LaserDefender/Assets/Scripts/Player.cs
+++ 06-LaserDefender/Assets/Scripts/Player.cs
@@ -14,9 +14,9 @@ public class Player : MonoBehaviour
 
     private void move()
     {
-        var delta = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        var delta = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         delta.Normalize();
         delta *= speed * Time.deltaTime;
-        transform.position = new Vector2(transform.position.x + delta.x, transform.position.y + delta.y);
+        transform.position += delta;
     }
 }
