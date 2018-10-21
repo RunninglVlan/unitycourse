57370614c8a8c850eaab5e5ce909e9a6ffecb979
RunninglVlan <RedCapShortCut@gmail.com>
2018-10-21 21:36:59 +0300

    Added player movement using both axes   Added Lecture notes

---
+++ 06-LaserDefender/Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] int speed = 10;
+
+    void Update()
+    {
+        move();
+    }
+
+    private void move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        var deltaY = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        transform.position = new Vector2(transform.position.x + deltaX, transform.position.y + deltaY);
+    }
+}
---
+++ 06-LaserDefender/README.md
@@ -0,0 +1,5 @@
+# Notes
+## What's new
+- [Input Manager](https://docs.unity3d.com/Manual/class-InputManager.html) has predefined input axes, e.g. Horizontal for X axis with LeftArrow and A as negative buttons
+  - [Input.GetAxis(axisName)](https://docs.unity3d.com/ScriptReference/Input.GetAxis.html)
+- Use [Time.deltaTime](https://docs.unity3d.com/ScriptReference/Time-deltaTime.html) to make the game frame rate independent
