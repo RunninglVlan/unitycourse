d625b372f7a7daae407325fbcd4c1588a55ad1a0
RunninglVlan <RedCapShortCut@gmail.com>
2018-10-21 22:24:36 +0300

    Added padding to movement boundaries   Removed unused sprite   Moved enemy a little

--- 06-LaserDefender/Assets/Scripts/Player.cs
+++ 06-LaserDefender/Assets/Scripts/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
 
     [SerializeField] int speed = 10;
+    [SerializeField] float playerPadding = 1;
 
     private Vector2 minBoundary;
     private Vector2 maxBoundary;
@@ -33,8 +34,8 @@ public class Player : MonoBehaviour
         delta.Normalize();
         delta *= speed * Time.deltaTime;
         var newPosition = transform.position + delta;
-        newPosition.x = Mathf.Clamp(newPosition.x, minBoundary.x, maxBoundary.x);
-        newPosition.y = Mathf.Clamp(newPosition.y, minBoundary.y, maxBoundary.y);
+        newPosition.x = Mathf.Clamp(newPosition.x, minBoundary.x + playerPadding, maxBoundary.x - playerPadding);
+        newPosition.y = Mathf.Clamp(newPosition.y, minBoundary.y + playerPadding, maxBoundary.y - playerPadding);
         transform.position = newPosition;
     }
 }
