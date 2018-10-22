be0b93578496df511a93eb8a938932362e99910d
RunninglVlan <RedCapShortCut@gmail.com>
2018-10-22 19:37:11 +0300

    Added path finding for enemies   Created first path prefab with 3 waypoints   Enemy moves towards next waypoint     Destroys itself on reaching last waypoint

---
+++ 06-LaserDefender/Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+
+    [SerializeField] List<Transform> waypoints = new List<Transform>();
+    [SerializeField] float movementSpeed = 2;
+
+    private int waypointIndex = 0;
+
+    void Start()
+    {
+        transform.position = waypoints[waypointIndex].position;
+    }
+
+    void Update()
+    {
+        move();
+    }
+
+    private void move()
+    {
+        if (waypointIndex < waypoints.Count)
+        {
+            var targetPosition = waypoints[waypointIndex].position;
+            var movementThisFrame = movementSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- 06-LaserDefender/README.md
+++ 06-LaserDefender/README.md
@@ -8,3 +8,4 @@
   - Some features depend on each other (Not much use from Enemy Health without Player Shooting), some can be grouped (Player Movement and Shooting). Some of the polish areas can be implemented in the middle.
 - [Quaternion](https://docs.unity3d.com/ScriptReference/Quaternion.html)s are used to represent rotations
 - [Coroutines](https://docs.unity3d.com/Manual/Coroutines.html) are like functions that have the ability to pause execution and return control to Unity but then to continue where they left off on the following frame or after specified time
+- [Vector2.MoveTowards](https://docs.unity3d.com/ScriptReference/Vector2.MoveTowards.html)
