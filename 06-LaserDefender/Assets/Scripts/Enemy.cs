8080e2ff146704acb108b6cdaf332e54512cf424
RunninglVlan <RedCapShortCut@gmail.com>
2018-10-23 22:04:05 +0300

    Crated DamageDealer and added it to Laser Prefab   Made Laser Trigger, disabled Trigger for Boudnaries   Laser now damages Enemies (created and attached script)     It also Destroy itself on contact   Added colliders to Enemies

---
+++ 06-LaserDefender/Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    [SerializeField] int health = 100;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        var damageDealer = other.GetComponent<DamageDealer>();
+        damageDealer.hit();
+        health -= damageDealer.damageDealt();
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
