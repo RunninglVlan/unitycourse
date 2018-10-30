1f577c7469f279bd435b7c11bbad568c57186be9
RunninglVlan <RedCapShortCut@gmail.com>
2018-10-30 20:36:12 +0200

    Added music theme, created singleton   The theme is Futureform by Sebby64 (Sean Lilley)     Found it on newgrounds long time ago   Made ensureSingleton method more general     We now get type of current object   Refactoring - moved script to folder

---
+++ 06-LaserDefender/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+
+    [SerializeField] float speed = .5f;
+
+    private Material material;
+    private Vector2 offset;
+
+    void Start()
+    {
+        material = GetComponent<Renderer>().material;
+        offset = new Vector2(0, speed);
+    }
+
+    void Update()
+    {
+        material.mainTextureOffset += offset * Time.deltaTime;
+    }
+}
