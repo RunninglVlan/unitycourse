a93ef91c6f64cdf2ef7789bbfc97383ad1411ae5
RunninglVlan <RedCapShortCut@gmail.com>
2018-10-31 11:26:04 +0200

    Created GameSession for showing score, made it Singleton   Refactoring - created prefabs for remaining game objects     It's easier to search in them this way

---
+++ 06-LaserDefender/Assets/Scripts/GameSession.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameSession : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI scoreTextComponent;
+
+    private int currentScore = 0;
+
+    void Awake()
+    {
+        ensureSingleton();
+    }
+
+    private void ensureSingleton()
+    {
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        showScore();
+    }
+
+    private void showScore()
+    {
+        scoreTextComponent.text = currentScore.ToString();
+    }
+}
