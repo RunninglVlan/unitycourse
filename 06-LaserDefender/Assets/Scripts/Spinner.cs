cd4183c77274a4065961bd6b44136c3aca8e6758
RunninglVlan <RedCapShortCut@gmail.com>
2018-11-01 13:36:38 +0200

    Made bombs spin, added sorting layer for Projectiles   Now Projectiles are rendered before spaceships

---
+++ 06-LaserDefender/Assets/Scripts/Spinner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+
+    [SerializeField] int speed = 45;
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, speed * Time.deltaTime));
+    }
+}
