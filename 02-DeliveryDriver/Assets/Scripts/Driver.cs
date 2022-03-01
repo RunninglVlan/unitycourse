3db5ed41a23425a317c08c88fa09e4865a6c246f
RunninglVlan <RedCapShortCut@gmail.com>
2022-03-02 00:54:06 +0200

    feat: completed 28. Boosts & Bumps
    Added move speed boosts and slow downs
    - Their effect fades away after time
    Decreased moveSpeed when moving backwards

--- 02-DeliveryDriver/Assets/Scripts/Driver.cs
+++ 02-DeliveryDriver/Assets/Scripts/Driver.cs
@@ -1,12 +1,22 @@
+using System.Collections;
 using UnityEngine;
 
 public class Driver : MonoBehaviour {
     [SerializeField] int steerSpeed = 20;
-    [SerializeField] int moveSpeed = 2;
+    [SerializeField] int normalMoveSpeed = 20;
+    [SerializeField] int slowMoveSpeed = 10;
+    [SerializeField] int fastMoveSpeed = 30;
+    [SerializeField] float moveSpeedEffectDuration = 2;
 
     Controls controls = null!;
+    int moveSpeed;
+    Coroutine? moveSpeedEffect;
 
-    void Awake() => controls = new Controls();
+    void Awake() {
+        controls = new Controls();
+        moveSpeed = normalMoveSpeed;
+    }
+
     void OnEnable() => controls.Enable();
     void OnDisable() => controls.Disable();
 
@@ -15,8 +25,36 @@ public class Driver : MonoBehaviour {
         var move = controls.Movement.Move.ReadValue<float>();
         if (move >= 0) {
             steer *= -1;
+        } else {
+            move /= 2;
         }
+
         transform.Rotate(Vector3.forward, steer * steerSpeed * Time.deltaTime);
         transform.Translate(0, move * moveSpeed * Time.deltaTime, 0);
+    }
+
+    void OnCollisionEnter2D(Collision2D _) {
+        ChangeMoveSpeed(slowMoveSpeed);
+    }
+
+    void OnTriggerEnter2D(Collider2D trigger) {
+        if (trigger.CompareTag("Boost")) {
+            ChangeMoveSpeed(fastMoveSpeed);
+        }
+    }
+
+    void ChangeMoveSpeed(int newSpeed) {
+        if (moveSpeedEffect != null) {
+            StopCoroutine(moveSpeedEffect);
+        }
+
+        moveSpeed = newSpeed;
+        moveSpeedEffect = StartCoroutine(ChangeMoveSpeedBackToNormal());
+    }
+
+    IEnumerator ChangeMoveSpeedBackToNormal() {
+        yield return new WaitForSeconds(moveSpeedEffectDuration);
+        moveSpeed = normalMoveSpeed;
+        moveSpeedEffect = null;
     }
 }
