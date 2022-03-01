e1b19ffb0e539606d316649b37762f318b91e35b
RunninglVlan <RedCapShortCut@gmail.com>
2022-03-01 18:34:45 +0200

    feat: completed 13. Transform.Translate()
    Now the car both rotates and translates

--- 02-DeliveryDriver/Assets/Driver.cs
+++ 02-DeliveryDriver/Assets/Driver.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour {
     void Update() {
-        transform.Rotate(Vector3.forward, 10 * Time.deltaTime);
+        transform.Rotate(Vector3.forward, 20 * Time.deltaTime);
+        transform.Translate(0, 2 * Time.deltaTime, 0);
     }
 }
