0a888b7a30c2868a2510e59576d615da5bfed2ce
RunninglVlan <RedCapShortCut@gmail.com>
2022-03-01 18:43:45 +0200

    feat: completed 15. How To Use SerializeField
    Extracted speed values into fields

--- 02-DeliveryDriver/Assets/Driver.cs
+++ 02-DeliveryDriver/Assets/Driver.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
 
 public class Driver : MonoBehaviour {
+    [SerializeField] int steerSpeed = 20;
+    [SerializeField] int moveSpeed = 2;
+
     void Update() {
-        transform.Rotate(Vector3.forward, 20 * Time.deltaTime);
-        transform.Translate(0, 2 * Time.deltaTime, 0);
+        transform.Rotate(Vector3.forward, steerSpeed * Time.deltaTime);
+        transform.Translate(0, moveSpeed * Time.deltaTime, 0);
     }
 }
