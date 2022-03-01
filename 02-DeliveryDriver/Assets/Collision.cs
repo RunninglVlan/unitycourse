ffa5fc70503a7c09ec7e743e820f1b99eec9ab2c
RunninglVlan <RedCapShortCut@gmail.com>
2022-03-01 20:06:59 +0200

    feat: completed 20. Using OnTriggerEnter2D()
    Added Rectangular Trigger

--- 02-DeliveryDriver/Assets/Collision.cs
+++ 02-DeliveryDriver/Assets/Collision.cs
@@ -4,4 +4,8 @@ public class Collision : MonoBehaviour {
     void OnCollisionEnter2D(Collision2D _) {
         Debug.Log("Ouch!");
     }
+
+    void OnTriggerEnter2D(Collider2D _) {
+        Debug.Log("Reached the zone");
+    }
 }
