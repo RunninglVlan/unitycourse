9acedc17fe90f830d04fd28010179e58414c1d2a
RunninglVlan <RedCapShortCut@gmail.com>
2022-03-01 23:51:39 +0200

    refactor

--- 02-DeliveryDriver/Assets/Scripts/Delivery.cs
+++ 02-DeliveryDriver/Assets/Scripts/Delivery.cs
@@ -7,20 +7,14 @@ public class Delivery : MonoBehaviour {
 
     bool hasPackage;
 
-    void OnCollisionEnter2D(Collision2D _) {
-        Debug.Log("Ouch!");
-    }
-
     void OnTriggerEnter2D(Collider2D trigger) {
         switch (hasPackage) {
             case false when trigger.CompareTag("Package"):
-                Debug.Log("Picked up the package");
                 Destroy(trigger.gameObject);
                 PackagePickedUp(trigger.transform.position);
                 hasPackage = true;
                 break;
             case true when trigger.CompareTag("Customer"):
-                Debug.Log("Delivered package to the customer");
                 Destroy(trigger.gameObject);
                 PackageDelivered(trigger.transform.position);
                 hasPackage = false;
