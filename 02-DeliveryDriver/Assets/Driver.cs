a87e25182d1e5d637af3f7b8a21410c652917f9a
RunninglVlan <RedCapShortCut@gmail.com>
2022-03-01 19:08:31 +0200

    feat: completed 16. Using Input.GetAxis()
    Added "com.unity.inputsystem": "1.3.0"
    Added 2 Axis actions

--- 02-DeliveryDriver/Assets/Driver.cs
+++ 02-DeliveryDriver/Assets/Driver.cs
@@ -4,8 +4,19 @@ public class Driver : MonoBehaviour {
     [SerializeField] int steerSpeed = 20;
     [SerializeField] int moveSpeed = 2;
 
+    Controls controls;
+
+    void Awake() => controls = new Controls();
+    void OnEnable() => controls.Enable();
+    void OnDisable() => controls.Disable();
+
     void Update() {
-        transform.Rotate(Vector3.forward, steerSpeed * Time.deltaTime);
-        transform.Translate(0, moveSpeed * Time.deltaTime, 0);
+        var steer = controls.Movement.Steer.ReadValue<float>();
+        var move = controls.Movement.Move.ReadValue<float>();
+        if (move >= 0) {
+            steer *= -1;
+        }
+        transform.Rotate(Vector3.forward, steer * steerSpeed * Time.deltaTime);
+        transform.Translate(0, move * moveSpeed * Time.deltaTime, 0);
     }
 }
