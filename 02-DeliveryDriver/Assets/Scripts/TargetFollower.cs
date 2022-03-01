5418869918bd34e2b18a133d80ed79fe549e4781
RunninglVlan <RedCapShortCut@gmail.com>
2022-03-01 21:45:35 +0200

    feat: completed 23. Simple Follow Camera

---
+++ 02-DeliveryDriver/Assets/Scripts/TargetFollower.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class TargetFollower : MonoBehaviour {
+    [SerializeField] Transform target;
+    [SerializeField] Vector3 shift;
+
+    void LateUpdate() {
+        transform.position = target.position + shift;
+    }
+}
