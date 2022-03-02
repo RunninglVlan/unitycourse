5f2bd711da1e2b35291298f96042aaad530c6689
RunninglVlan <RedCapShortCut@gmail.com>
2022-03-02 20:03:53 +0200

    feat: completed 40. Using Invoke() For Delays

--- 03-SnowBoarder/Assets/Scripts/Game.cs
+++ 03-SnowBoarder/Assets/Scripts/Game.cs
@@ -4,23 +4,24 @@ using UnityEngine.SceneManagement;
 public class Game : MonoBehaviour {
     [SerializeField] CrashDetection crashDetection = null!;
     [SerializeField] Finish finish = null!;
+    [SerializeField] float reloadDelay = 1;
 
     void Awake() {
         crashDetection.PlayerCrashed += Lose;
         finish.PlayerReached += Win;
     }
 
-    static void Lose() {
+    void Lose() {
         Debug.Log("Severe injury. Went to the hospital");
-        Reload();
+        Invoke(nameof(Reload), reloadDelay);
     }
 
-    static void Win() {
+    void Win() {
         Debug.Log("Hooray! We won");
-        Reload();
+        Invoke(nameof(Reload), reloadDelay);
     }
 
-    static void Reload() {
+    void Reload() {
         var activeScene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(activeScene.buildIndex);
     }
