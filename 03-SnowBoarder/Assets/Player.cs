182c5b8c0e56bcb750ba32c12689bd55d341df13
RunninglVlan <RedCapShortCut@gmail.com>
2022-03-02 19:09:25 +0200

    feat: completed 37. AddTorque To Rotate
    Added "com.unity.inputsystem": "1.3.0"

---
+++ 03-SnowBoarder/Assets/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour {
+    [SerializeField] float torqueAmount = 1;
+
+    Controls controls = null!;
+    Rigidbody2D body = null!;
+
+    void Awake() {
+        controls = new Controls();
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void OnEnable() => controls.Enable();
+    void OnDisable() => controls.Disable();
+
+    void Update() {
+        var torque = controls.Movement.Torque.ReadValue<float>();
+        if (!Mathf.Approximately(torque, 0)) {
+            body.AddTorque(torque * torqueAmount);
+        }
+    }
+}
