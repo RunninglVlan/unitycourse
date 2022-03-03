8ec88e6f42a64e8f6107f1042b8bb0b41e67270d
RunninglVlan <RedCapShortCut@gmail.com>
2022-03-03 17:45:17 +0200

    feat: completed 45. Using OnCollisionExit2D

---
+++ 03-SnowBoarder/Assets/Scripts/DustTrail.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class DustTrail : MonoBehaviour {
+    ParticleSystem particles = null!;
+
+    void Awake() => particles = GetComponentInChildren<ParticleSystem>();
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.CompareTag("Ground")) {
+            particles.Play();
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other) {
+        if (other.gameObject.CompareTag("Ground")) {
+            particles.Stop();
+        }
+    }
+}
