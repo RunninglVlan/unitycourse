f62f731e34fd1c8ed4d925e0a034037b8a55dadc
RunninglVlan <RedCapShortCut@gmail.com>
2022-03-03 17:00:06 +0200

    feat: completed 41. Introducing Particle Effects

--- 03-SnowBoarder/Assets/Scripts/CrashDetection.cs
+++ 03-SnowBoarder/Assets/Scripts/CrashDetection.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 public class CrashDetection : MonoBehaviour {
     public event Action PlayerCrashed = delegate { };
 
+    ParticleSystem particles = null!;
+
+    void Awake() => particles = GetComponentInChildren<ParticleSystem>();
+
     void OnTriggerEnter2D(Collider2D trigger) {
-        if (trigger.CompareTag("Level")) {
-            PlayerCrashed();
+        if (!trigger.CompareTag("Level")) {
+            return;
         }
+
+        particles.Play();
+        PlayerCrashed();
     }
 }
--- 03-SnowBoarder/Assets/Scripts/Finish.cs
+++ 03-SnowBoarder/Assets/Scripts/Finish.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 public class Finish : MonoBehaviour {
     public event Action PlayerReached = delegate { };
 
+    ParticleSystem particles = null!;
+
+    void Awake() => particles = GetComponentInChildren<ParticleSystem>();
+
     void OnTriggerEnter2D(Collider2D trigger) {
-        if (trigger.CompareTag("Player")) {
-            PlayerReached();
+        if (!trigger.CompareTag("Player")) {
+            return;
         }
+
+        particles.Play();
+        PlayerReached();
     }
 }
